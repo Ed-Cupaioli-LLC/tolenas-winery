c6a6f69b92cd60d76ca64308591d479b0884d87e
Ed Cupaioli <ecupaio@gmail.com>
2021-01-19 17:15:39 -0800

    added default pages

--- js/site_functions.js
+++ js/site_functions.js
@@ -1,2 +1,11 @@
 $(function(){
+  $.ajax({
+    url: "https://api.commerce7.com/v1/product-for-web?collectionSlug=wine&page=1",
+    type: 'GET',
+    headers: {"tenant": "tolenas-vineyards-and-winery"},
+    
+    success: function(data) {
+      console.log(data);
+    }
+  })
 });
