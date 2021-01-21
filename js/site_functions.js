03aa954ea580090d208bf65c121f3a6088d070ba
Ed Cupaioli <ecupaio@gmail.com>
2021-01-21 13:05:55 -0800

    nav

--- js/site_functions.js
+++ js/site_functions.js
@@ -1,11 +1,21 @@
 $(function(){
-  $.ajax({
-    url: "https://api.commerce7.com/v1/product-for-web?collectionSlug=wine&page=1",
-    type: 'GET',
-    headers: {"tenant": "tolenas-vineyards-and-winery"},
+  // $.ajax({
+  //   url: "https://api.commerce7.com/v1/product-for-web?collectionSlug=wine&page=1",
+  //   type: 'GET',
+  //   headers: {"tenant": "tolenas-vineyards-and-winery"},
     
-    success: function(data) {
-      console.log(data);
+  //   success: function(data) {
+  //     console.log(data);
+  //   }
+  // });
+  $(window).scroll(function() {
+    if ($(this).scrollTop() > 88) { 
+      $('nav').addClass('scrolled');
+    } else {
+      $('nav').removeClass('scrolled');
     }
-  })
+  });
+  $('.nav-toggle').click(function() {
+    $('nav').toggleClass('active');
+  });
 });
