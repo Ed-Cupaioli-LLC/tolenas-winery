ef75cca257797a7d47ad20f2185dbc3be33ed30d
Ed Cupaioli <ecupaio@gmail.com>
2021-01-25 17:09:05 -0800

    added visit mobile layout

--- js/visit.js
+++ js/visit.js
@@ -7,13 +7,25 @@ $(function (){
 
     $('.menu-link.active').removeClass('active');
     $(this).addClass('active');
-    
+    $('#visit-menu').removeClass('open');
+    const offsetTop;
+    if ($(window).width() <= 1024) {
+      offsetTop = 150;
+      
+    } else {
+      offsetTop = 88;
+    }
     $('html,body').animate({
-      scrollTop: $(anchor).offset().top - 88
+      scrollTop: $(anchor).offset().top - offsetTop
     });
   });
   $(window).on('load scroll', function() {
     checkOffset('#visit-menu','footer');
+    if ($(this).scrollTop() > 88 && $(this).width() <= 1024) { 
+      $('#visit-menu').addClass('scrolled');
+    }  else {
+      $('#visit-menu').removeClass('scrolled');
+    }
   });
   function checkOffset(fixedObj,footer) {
     if($(fixedObj).offset().top + $(fixedObj).height() >= $(footer).offset().top - 10)
@@ -22,4 +34,7 @@ $(function (){
     if($(document).scrollTop() + window.innerHeight < $(footer).offset().top)
     $(fixedObj).removeClass("at-footer");
   }
+  $('.visit-menu-toggle').click(function () {
+    $('#visit-menu').toggleClass('open')
+  });
 });
