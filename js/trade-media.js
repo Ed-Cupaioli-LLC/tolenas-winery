c1eebd028ff7fe1e9c85cb1942c6d55ef75a7942
Ed Cupaioli <ecupaio@gmail.com>
2021-02-02 13:58:58 -0800

    added trade media content

---
+++ js/trade-media.js
@@ -0,0 +1,21 @@
+$(function (){
+  //select category
+  $('.category-menu .menu-link').click(function(e) {
+    e.preventDefault();
+    $('.category-menu .menu-link.active').removeClass('active');
+    $(this).addClass('active');
+    const activeSection = $(this).attr('href');
+    $('.content-section.active').removeClass('active');
+    $(activeSection).addClass('active');
+  });
+  //select content block
+  $('.content-nav .nav-link').click(function(e){
+    e.preventDefault();
+    $(this).parents('.content-nav').find('.nav-link.active').removeClass('active');
+    $(this).addClass('active');
+
+    const activeBlock = $(this).attr('href');
+    $('.content-block.active').removeClass('active');
+    $(activeBlock).addClass('active');
+  });
+});
