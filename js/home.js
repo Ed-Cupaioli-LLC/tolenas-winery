2ef910d2964d68457d436ac9d607f4a79a7d76a8
Ed Cupaioli <ecupaio@gmail.com>
2021-04-07 12:45:06 -0700

    merge conflict bs

--- js/home.js
+++ js/home.js
@@ -41,51 +41,102 @@ $(function(){
     }
   });
   //insta feed
-  $.ajax({
-    url: 'https://www.instagram.com/graphql/query/?query_hash=472f257a40c653c64c666ce877d59d2b&variables={%22id%22:%227741927326%22,%22first%22:12}',
-    type: 'GET',
-    dataType: 'json',
-    success: function(data) {
-      var igFeed = data.data.user.edge_owner_to_timeline_media.edges;
+  // $.ajax({
+  //   url: 'https://www.instagram.com/graphql/query/?query_hash=472f257a40c653c64c666ce877d59d2b&variables={%22id%22:%227741927326%22,%22first%22:12,%22after%22:null}',
+  //   type: 'GET',
+  //   dataType: 'json',
+  //   success: function(data) {
+  //     var igFeed = data.data.user.edge_owner_to_timeline_media.edges;
+  //     $(igFeed).each(function(i,obj) {
+  //       var igPost = obj.node;
+  //       var imgThumb = obj.node.thumbnail_src;
+  //       var imgFull = obj.node.display_url;
+  //       var imgAlt = obj.node.accessibility_caption;
+  //       var shortCode = obj.node.shortcode;
+  //       var igText = igPost.edge_media_to_caption.edges[0].node.text;
+  //       var slide = '<div class="insta-slide" data-shortcode="'+shortCode+'" data-imagefull="'+imgFull+'" >'+
+  //                     '<div class="slide-content">'+
+  //                       '<img alt="'+imgAlt+'" src="'+imgThumb+'" class="slide-img" />'+
+  //                       '<div class="slide-text hidden">'+igText.replace(/\n/g, "<br />")+'</div>'+
+  //                     '</div>'+
+  //                   '<div>';  
+  //       $('#ig-slider').append(slide);
+  //     });  
+  //     $('#ig-slider').slick({
+  //       slidesToShow: 3,
+  //       responsive: [
+  //         {
+  //           breakpoint: 768,
+  //           settings: {
+  //             slidesToShow: 1
+  //           }
+  //         }
+  //       ]
+  //     });  
+  //     $('.insta-slide').click(function() {
+  //       $('.post-embed-container,body,html').addClass('active');
+  //       var postCode = $(this).data('shortcode');
+  //       var postLink = 'https://www.instagram.com/p/'+postCode;
+  //       var postText = $(this).find('.slide-text').html();
+  //       var postImage = $(this).data('imagefull');
+  //       $('.insta-image').attr('src',postImage);
+  //       $('.insta-caption').html(postText);
+  //       $('.insta-link').attr('href','https://www.instagram.com/p/'+postCode);
+  //     }); 
+  //   }, error: function(data) {    
+  //     console.log(data);  
+  //   }
+  // });
+  $.instagramFeed({
+    'username': 'tolenaswinery',
+    //'container': "#ig-slider",
+    'display_profile': false,
+    'display_biography': false,
+    'display_gallery': true,
+    'display_captions': false,
+    'callback': function(data) {
+      var igFeed = data.edge_owner_to_timeline_media.edges;
       $(igFeed).each(function(i,obj) {
-        var igPost = obj.node;
-        var imgThumb = obj.node.thumbnail_src;
-        var imgFull = obj.node.display_url;
-        var imgAlt = obj.node.accessibility_caption;
-        var shortCode = obj.node.shortcode;
-        var igText = igPost.edge_media_to_caption.edges[0].node.text;
-        var slide = '<div class="insta-slide" data-shortcode="'+shortCode+'" data-imagefull="'+imgFull+'" >'+
-                      '<div class="slide-content">'+
-                        '<img alt="'+imgAlt+'" src="'+imgThumb+'" class="slide-img" />'+
-                        '<div class="slide-text hidden">'+igText.replace(/\n/g, "<br />")+'</div>'+
-                      '</div>'+
-                    '<div>';  
-        $('#ig-slider').append(slide);
-      });  
-      $('#ig-slider').slick({
-        slidesToShow: 3,
-        responsive: [
-          {
-            breakpoint: 768,
-            settings: {
-              slidesToShow: 1
-            }
-          }
-        ]
-      });  
-      $('.insta-slide').click(function() {
-        $('.post-embed-container,body,html').addClass('active');
-        var postCode = $(this).data('shortcode');
-        var postLink = 'https://www.instagram.com/p/'+postCode;
-        var postText = $(this).find('.slide-text').html();
-        var postImage = $(this).data('imagefull');
-        $('.insta-image').attr('src',postImage);
-        $('.insta-caption').html(postText);
-        $('.insta-link').attr('href','https://www.instagram.com/p/'+postCode);
-      }); 
-    }, error: function(data) {    
-      console.log(data);  
-    }
+              var igPost = obj.node;
+              var imgThumb = obj.node.thumbnail_src;
+              var imgFull = obj.node.display_url;
+              var imgAlt = obj.node.accessibility_caption;
+              var shortCode = obj.node.shortcode;
+              var igText = igPost.edge_media_to_caption.edges[0].node.text;
+              var slide = '<div class="insta-slide" data-shortcode="'+shortCode+'" data-imagefull="'+imgFull+'" >'+
+                            '<div class="slide-content">'+
+                              '<img alt="'+imgAlt+'" src="'+imgThumb+'" class="slide-img" />'+
+                              '<div class="slide-text hidden">'+igText.replace(/\n/g, "<br />")+'</div>'+
+                            '</div>'+
+                          '<div>';  
+              $('#ig-slider').append(slide);
+            });  
+            $('#ig-slider').slick({
+              slidesToShow: 3,
+              responsive: [
+                {
+                  breakpoint: 768,
+                  settings: {
+                    slidesToShow: 1
+                  }
+                }
+              ]
+            });  
+            $('.insta-slide').click(function() {
+              $('.post-embed-container,body,html').addClass('active');
+              var postCode = $(this).data('shortcode');
+              var postLink = 'https://www.instagram.com/p/'+postCode;
+              var postText = $(this).find('.slide-text').html();
+              var postImage = $(this).data('imagefull');
+              $('.insta-image').attr('src',postImage);
+              $('.insta-caption').html(postText);
+              $('.insta-link').attr('href','https://www.instagram.com/p/'+postCode);
+            }); 
+    },
+    'styling': true,
+    'items': 3,
+    'items_per_row': 3,
+    'margin': 1 
   });
   $('.close-embed').click(function() {
     $('.post-embed-container,body,html').removeClass('active');
