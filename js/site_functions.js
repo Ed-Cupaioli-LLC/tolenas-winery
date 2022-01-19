59790dfdeeb4f26ee66f79a836aee652df4d5096
Ed Cupaioli <ecupaio@gmail.com>
2022-01-19 10:12:10 -0800

    removed custom signup form code

--- js/site_functions.js
+++ js/site_functions.js
@@ -9,42 +9,6 @@ $(function(){
   $('.nav-toggle').click(function() {
     $('nav').toggleClass('active');
   });
-  //signup form
-  $('.subscribe-form').submit(function(e) {
-    e.preventDefault();
-    const email = $(this).find('.email').val()
-    const emailSub = {
-      "email": email
-    };
-    $.ajax({
-      url: "https://api.commerce7.com/v1/customer/subscribe",
-      beforeSend: function(xhr) {
-        xhr.setRequestHeader("tenant", "tolenas-vineyards-and-winery");
-        xhr.setRequestHeader("Content-Type", "application/json");
-      },
-      dataType: 'json',
-      data: JSON.stringify(emailSub),
-      type: 'POST',
-      success: function(data) {
-        $(this).find('.result-msg').text('Thanks for signing up!');
-        $(this).find('.form-body').slideUp();
-        console.log(data);
-      },
-      error: function(data) {
-        var errorData = $.parseJSON(data.responseText);
-        var errors = errorData.errors;
-        console.log(data);
-        $(errors).each(function(i,err) {
-          
-          $(this).find('.result-msg').text(err.message);
-          $(this).find('.result-msg').slideDown();
-        });
-      }
-      
-    }).done(function(){
-      $('#subscribe-form .result-msg').slideDown();
-    });
-  });
   
   //hero animation 
   if ($('#hero').length > 0) {
